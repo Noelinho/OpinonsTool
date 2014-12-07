d3127cc23c84d2c84ba1bb1ce720245d64778b7c
Joaquín <jjtarrago@gmail.com>
2014-12-07 13:27:02 +0100

    filtering

--- www/app/views/ListOpinions.js
+++ www/app/views/ListOpinions.js
@@ -27,42 +27,9 @@ define([
             });
         },
         events: {
-            'click #validar-opinion': 'validarOpinion',
-            'click #invalidar-opinion': 'invalidarOpinion',
-            'click #save-opinion': 'saveOpinion',
             'click #inlineRadio1': 'showAll',
             'click #inlineRadio2': 'showValid',
             'click #inlineRadio3': 'showInvalid'
-        },
-
-        validarOpinion: function(e) {
-            e.preventDefault();
-            var idOpinion = $(e.target).data('idopinion');
-
-            var model = this.collection.get(idOpinion);
-            model.set('_status', '20');
-
-            model.save();
-        },
-        invalidarOpinion: function(e) {
-            e.preventDefault();
-            var idOpinion = $(e.target).data('idopinion');
-
-            var model = this.collection.get(idOpinion);
-            model.set('_status', '10');
-
-            model.save();
-        },
-        saveOpinion: function(e) {
-            e.preventDefault();
-            var idOpinion = $(e.target).data('idopinion');
-
-            var model = this.collection.get(idOpinion);
-            model.set('_mejor', $('#mejor_' + idOpinion).val());
-            model.set('_peor', $('#peor_' + idOpinion).val());
-            model.set('_cuerpo', $('#cuerpo_' + idOpinion).val());
-
-            model.save();
         },
         showValid:function(e) {
             this.filter = '20';
--- www/app/views/Opinion.js
+++ www/app/views/Opinion.js
@@ -15,6 +15,34 @@ define([
 
             initialize: function() {
                 this.listenTo(this.model, 'change', this.render);
+            },
+
+            events: {
+                'click #validar-opinion': 'validarOpinion',
+                'click #invalidar-opinion': 'invalidarOpinion',
+                'click #save-opinion': 'saveOpinion'
+            },
+
+            validarOpinion: function(e) {
+                e.preventDefault();
+
+                this.model.set('_status', '20');
+                this.model.save();
+            },
+            invalidarOpinion: function(e) {
+                e.preventDefault();
+
+                this.model.set('_status', '10');
+                this.model.save();
+            },
+            saveOpinion: function(e) {
+                e.preventDefault();
+
+                this.model.set('_mejor', $('#mejor_' + this.model.id).val());
+                this.model.set('_peor', $('#peor_' + this.model.id).val());
+                this.model.set('_cuerpo', $('#cuerpo_' + this.model.id).val());
+
+                this.model.save();
             }
 
 
